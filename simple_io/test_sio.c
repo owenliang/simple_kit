eba3abb2de72a72badba800bc0363a4c915bb722
owenliang <owenliang@users.noreply.github.com>
2014-07-29 22:49:43 +0800

    update codes

--- simple_io/test_sio.c
+++ simple_io/test_sio.c
@@ -15,51 +15,91 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
 #include "sio.h"
+
+static char cmdline_quit = 0;
+
+static void stdin_echo(struct sio_fd *sfd)
+{
+	char input[64];
+	int ret = read(sfd->fd, input, sizeof(input));
+	if (ret > 0) {
+		printf("%.*s", ret, input);
+	} else if (ret == 0) {
+		printf("stdin_echo=read:EOF\n");
+		cmdline_quit = 1;
+	} else {
+		printf("stdin_echo=read:-1\n");
+	}
+}
 
 static void stdin_callback(struct sio *sio, struct sio_fd *sfd, enum sio_event event, void *arg)
 {
     switch (event) {
     case SIO_READ:
-        fprintf(stderr, "SIO_READ\n");
+        stdin_echo(sfd);
         break;
     case SIO_WRITE:
-        fprintf(stderr, "SIO_WRITE\n");
+        fprintf(stderr, "stdin_callback=write\n");
         break;
     case SIO_ERROR:
-        fprintf(stderr, "SIO_ERROR\n");
+        fprintf(stderr, "stdin_callback=error\n");
         break;
     default:
-        fprintf(stderr, "unknown event\n");
+        fprintf(stderr, "stdin_callback=unknown\n");
         break;
     }
 }
 
 static void timer_callback(struct sio *sio, struct sio_timer *timer, void *arg)
 {
-    fprintf(stderr, "timer_callback\n");
+    fprintf(stderr, "-------------------[timer]2 seconds passed------------------\n");
     sio_start_timer(sio, timer, 2000, timer_callback, NULL);
+}
+
+static void sio_cmdline_quit_handler(int signo)
+{
+	cmdline_quit = 1;
+}
+
+static void sio_cmdline_signal()
+{
+	struct sigaction act;
+	memset(&act, 0, sizeof(act));
+	act.sa_handler = sio_cmdline_quit_handler;
+	sigaction(SIGINT, &act, NULL);
+	sigaction(SIGTERM, &act, NULL);
 }
 
 int main(int argc, char **argv)
 {
+	sio_cmdline_signal();
+
     struct sio *sio = sio_new();
     if (!sio) {
-        fprintf(stderr, "sio_new returns NULL\n");
+        fprintf(stderr, "sio_new=NULL\n");
         return -1;
     }
+
     struct sio_fd *sfd = sio_add(sio, 0, stdin_callback, NULL);
     if (!sfd) {
-        fprintf(stderr, "sio_add returns NULL\n");
+        fprintf(stderr, "sio_add=NULL\n");
         return -1;
     }
     sio_watch_read(sio, sfd);
+
     struct sio_timer timer;
     sio_start_timer(sio, &timer, 2000, timer_callback, NULL);
-    int i;
-    for (i = 0; i < 10; ++i) 
+
+    while (!cmdline_quit)
         sio_run(sio, 1000);
+
     sio_del(sio, sfd);
     sio_stop_timer(sio, &timer);
     sio_free(sio);
